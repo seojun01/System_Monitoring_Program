646c5c7695e1a4b74eb285851775453029c20cc3
seojun01 <parkseojoon@naver.com>
2023-08-08 15:15:07 +0900

    Setting Proxy

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const callApi = async () => {
+        axios.get('/api').then((res) => {
+            console.log('API Loaded');
+        });
+    };
+
+    useEffect(() => {
+        callApi();
+    }, []);
+
+    return (
+        <div className="App">
+            <h1>API Testing</h1>
+        </div>
+    );
 }
 
 export default App;
--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('/api')
+  getAPI(): boolean {
+    return this.appService.getAPI();
+  }
 }
