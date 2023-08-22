10a15016bb6c065bf08df61c75239b0fd8fc762d
김태정 <kimtaejung1053@gmail.com>
2023-08-22 18:49:05 +0900

    Frontend-Mainpages

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -9,7 +9,7 @@ function Header(): JSX.Element {
     return (
         <nav className="sb-topnav navbar navbar-expand navbar-dark bg-dark">
             <a className="navbar-brand ps-3" href="/">
-                Log-Program
+                Log-Monitoring
             </a>
             <a
                 className="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0"
