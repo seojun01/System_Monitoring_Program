51c1747a987520d7b955cacd99f5cb7f47acade3
김태정 <kimtaejung1053@gmail.com>
2023-08-25 22:35:14 +0900

    Frontend-1stEdit of charts

--- client/src/pages/Menupages/System_Info.tsx
+++ client/src/pages/Menupages/System_Info.tsx
@@ -1,6 +1,170 @@
 import './pages.css';
+import ReactApexChart from 'react-apexcharts';
 
-function System_Info(): JSX.Element {
+function Cpu(): any {
+    const chart1: any = {
+        options: {
+            chart: {
+                height: 350,
+                type: 'area',
+                zoom: {
+                    enabled: false,
+                },
+            },
+            dataLabels: {
+                enabled: false,
+            },
+            stroke: {
+                curve: 'smooth',
+            },
+            title: {
+                text: 'CPU',
+                align: 'left',
+            },
+            xaxis: {
+                type: 'datetime',
+                categories: [
+                    '2018-09-19T00:00:00.000Z',
+                    '2018-09-19T01:00:00.000Z',
+                    '2018-09-19T02:00:00.000Z',
+                    '2018-09-19T03:00:00.000Z',
+                    '2018-09-19T04:00:00.000Z',
+                    '2018-09-19T05:00:00.000Z',
+                    '2018-09-19T06:00:00.000Z',
+                ],
+            },
+            tooltip: {
+                x: {
+                    format: 'dd/MM/yy HH:mm',
+                },
+            },
+            yaxis: [
+                {
+                    title: {
+                        text: 'Percentage',
+                    },
+                    labels: {
+                        formatter: function (value0: any): string {
+                            return value0 + '%';
+                        },
+                    },
+                },
+                {
+                    opposite: true,
+                    title: {
+                        text: 'Celsius',
+                    },
+                },
+            ],
+            grid: {
+                show: false,
+            },
+        },
+        series: [
+            {
+                name: 'USAGE',
+                data: [31, 40, 28, 51, 42, 90, 70],
+            },
+            {
+                name: 'TEMP',
+                data: [60, 65, 64, 60, 67, 70, 68],
+            },
+        ],
+    };
+
+    const chart2: any = {
+        series: [67],
+        options: {
+            chart: {
+                height: 350,
+                type: 'radialBar',
+                offsetY: 10,
+            },
+            plotOptions: {
+                radialBar: {
+                    startAngle: -135,
+                    endAngle: 135,
+                    dataLabels: {
+                        name: {
+                            fontSize: '16px',
+                            color: undefined,
+                            offsetY: 120,
+                        },
+                        value: {
+                            offsetY: 76,
+                            fontSize: '22px',
+                            color: undefined,
+                            formatter: function (val: number) {
+                                return val + '%';
+                            },
+                        },
+                    },
+                },
+            },
+            fill: {
+                type: 'gradient',
+                gradient: {
+                    shade: 'dark',
+                    shadeIntensity: 0.15,
+                    inverseColors: false,
+                    opacityFrom: 1,
+                    opacityTo: 1,
+                    stops: [0, 50, 65, 91],
+                },
+            },
+            stroke: {
+                dashArray: 4,
+            },
+            labels: ['메모리 사용량'],
+        },
+    };
+
+    const chart3: any = {
+        series: [33],
+        options: {
+            chart: {
+                height: 350,
+                type: 'radialBar',
+                offsetY: 10,
+            },
+            plotOptions: {
+                radialBar: {
+                    startAngle: -135,
+                    endAngle: 135,
+                    dataLabels: {
+                        name: {
+                            fontSize: '16px',
+                            color: undefined,
+                            offsetY: 120,
+                        },
+                        value: {
+                            offsetY: 76,
+                            fontSize: '22px',
+                            color: undefined,
+                            formatter: function (val: number) {
+                                return val + '%';
+                            },
+                        },
+                    },
+                },
+            },
+            fill: {
+                type: 'gradient',
+                gradient: {
+                    shade: 'dark',
+                    shadeIntensity: 0.15,
+                    inverseColors: false,
+                    opacityFrom: 1,
+                    opacityTo: 1,
+                    stops: [0, 50, 65, 91],
+                },
+            },
+            stroke: {
+                dashArray: 4,
+            },
+            labels: ['메모리 잔여량'],
+        },
+    };
     return (
         <div id="layoutSidenav">
             <div id="layoutSidenav_content">
@@ -11,10 +175,31 @@ function System_Info(): JSX.Element {
                             <li className="breadcrumb-item active">GRAPH</li>
                         </ol>
                     </div>
+                    <div id="chart">
+                        <div id="chart1">
+                            <ReactApexChart options={chart1.options} series={chart1.series} type="area" height={280} />
+                        </div>
+                        <div id="chart2">
+                            <ReactApexChart
+                                options={chart2.options}
+                                series={chart2.series}
+                                type="radialBar"
+                                height={350}
+                            />
+                        </div>
+                        <div id="chart3">
+                            <ReactApexChart
+                                options={chart3.options}
+                                series={chart3.series}
+                                type="radialBar"
+                                height={350}
+                            />
+                        </div>
+                    </div>
                 </main>
             </div>
         </div>
     );
 }
 
-export default System_Info;
+export default Cpu;
