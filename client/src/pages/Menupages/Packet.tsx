4fdbde7b185afe3c484b8c6caa1ad41f9e56a89a
“Hanamru1” <h01072223723@gmail.com>
2023-08-28 09:28:29 +0900

    front packet graph finished version

--- client/src/pages/Menupages/Packet.tsx
+++ client/src/pages/Menupages/Packet.tsx
@@ -1,125 +1,192 @@
-import ApexCharts from "react-apexcharts";
-import Chart from "react-apexcharts";
+import React from 'react';
+import ReactApexChart from 'react-apexcharts';
 import './pages.css';
 
 function Packet(): JSX.Element {
+    const chart1: any = {
+        options: {
+            chart: {
+                background: '#ffffff',
+                toolbar: {
+                    show: false,
+                },
+                zoom: {
+                    enabled: false,
+                },
+            },
+            title: {
+                text: 'Packet',
+                align: 'left',
+                style: {
+                    color: 'black',
+                },
+            },
+            xaxis: {
+                categories: ['12:39', '12:40', '12:41', '12:42', '12:43'],
+            },
+        },
+
+        series: [
+            {
+                name: '1',
+                data: [16, 20, 10, 9, 20],
+            },
+            {
+                name: '2',
+                data: [19, 25, 20, 9, 2],
+            },
+            {
+                name: '3',
+                data: [10, 23, 40, 9, 30],
+            }
+        ],
+    };
+
+    const chart2: any = {
+        options: {
+            chart: {
+                background: '#ffffff',
+                toolbar: {
+                    show: false,
+                },
+            },
+            title: {
+                text: 'Type of attack',
+                align: 'left',
+                style: {
+                    color: 'black',
+                },
+            },
+            xaxis: {
+                categories: ['SQL 인젝션', '커맨드 인젝션', 'Xpath 인젝션', 'XSS', 'CSRF'],
+            },
+        },
+        series: [
+            {
+                name: '공격종류',
+                data: [19, 25, 20, 9, 2],
+            },
+        ],
+    };
+
+    const chart3: any = {
+        options: {
+            chart: {
+                background: '#ffffff',
+                width: 380,
+            },
+            title: {
+                text: 'Attack Risk',
+                align: 'left',
+                style: {
+                    color: 'black',
+                },
+            },
+            labels: ['관심', '주의', '경고', '위험'],
+            responsive: [
+                {
+                    breakpoint: 480,
+                    options: {
+                        toolbar: {
+                            show: false,
+                        },
+                    },
+                },
+            ],
+        },
+        name: '위험도',
+        series: [44, 55, 13, 13],
+    };
+
+    const chart4: any = {
+        options: {
+            chart: {
+                background: '#ffffff',
+                toolbar: {
+                    show: false,
+                },
+            },
+            title: {
+                text: 'Number of attacks',
+                align: 'left',
+                style: {
+                    color: 'black',
+                },
+            },
+
+            responsive: [
+                {
+                    breakpoint: 480,
+                    options: {
+                        chart: {
+                            width: 200,
+                        },
+                        legend: {
+                            position: 'bottom',
+                        },
+                    },
+                },
+            ],
+            xaxis: {
+                categories: ['8:21', '8:22', '8:23', '8:24', '8:25', '8:26', '8.27'],
+            },
+            colors: ['#D25A5A'],
+        },
+        series: [
+            {
+                name: '공격횟수',
+                data: [19, 25, 20, 9, 2, 5, 8],
+            },
+        ],
+    };
+
     return (
         <div id="layoutSidenav">
-            <div id="layoutSidenav_content">
+            <div id="layoutSidenav_content" style={{ backgroundColor: '#f2f2f2', height: '169vh', }}>
                 <main>
                     <div className="container-fluid px-4">
-                        <h1 className="mt-4">Dashboard</h1>
+                        <h1 className="mt-4" style={{
+                            textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)',
+                            fontFamily: 'Arial, sans-serif',
+                            fontWeight: 'bold',
+                            fontSize: '2rem',
+                            color: '#333'
+                        }}>Dashboard</h1>
                         <ol className="breadcrumb mb-4">
                             <li className="breadcrumb-item active"></li>
-                            <div style={{ width: '1000%', height: '10%' }}>
-                                        <ApexCharts
-                                            type="area"
-                                            series={[
-                                                {
-                                                    name: "패킷 총량",
-                                                    data: [19, 25, 20, 9,2],
-                                                },
-                                            ]}
-                                            options={{
-                                                chart : {
-                                                    height: '20%',
-                                                    width: 900,
-                                                },
-                                                title: {
-                                                    text: 'Packet',
-                                                    align: 'left',
-                                                    style: {
-                                                      color: 'black',
-                                                    },
-                                                  },
-                                                xaxis: {
-                                                    categories:['12:39','12:40','12:41','12:42','12:43']
-                                                }
-                                            
-                                            }}
-                                        />
-                                </div>
-                                <div style={{ width: '38%', height: '50%',position: 'absolute',top: '100%', left: '10%'}}>
-                                        <ApexCharts
-                                            type="radar"
-                                            series={[
-                                                {
-                                                    name: "패킷 총량",
-                                                    data: [19, 25, 20, 9,2],
-                                                },
-                                            ]}
-                                            options={{
-                                                chart : {
-                                                    height: 100,
-                                                    width: 300
-                                                },
-                                                xaxis: {
-                                                    categories:['SQL 인젝션','커맨드 인젝션','Xpath 인젝션','XSS','CSRF']
-                                                }
-                                            
-                                            }}
-                                        />
-                                </div>
-                                <div style={{ width: '26%', height: '50%',position: 'absolute',top: '100%', left: '46%' }}>
-                                <ApexCharts
-                                    type="pie"
-                                    series={[44, 55, 13, 13]}
-                                    options={{
-                                        chart: {
-                                            width: 380,
-                                        },
-                                        labels: ['관심', '주의', '경고', '위험'],
-                                        responsive: [{
-                                            breakpoint: 480,
-                                            options: {
-                                                chart: {
-                                                    width: 200,
-                                                },
-                                                legend: {
-                                                    position: 'bottom',
-                                                },
-                                            },
-                                        }],
-                                    }}
-                                />
-                            </div>
-                            <div style={{ width: '29%', height: '50%',position: 'absolute',top: '100%', left: '70%' }}>
-                                <ApexCharts
-                                    type="bar"
-                                    series={[
-                                        {
-                                            name: '정상패킷',
-                                            data: [76, 85, 101, 98, 87, 105, 91]
-                                        }, {
-                                            name: '공격패킷',
-                                            data: [35, 41, 36, 26, 45, 48, 52]
-                                            
-                                        },
-                                    ]}
-                                    options={{
-                                        chart: {
-                                            width: 380,
-                                        },
-                                        responsive: [{
-                                            breakpoint: 480,
-                                            options: {
-                                                chart: {
-                                                    width: 200,
-                                                },
-                                                legend: {
-                                                    position: 'bottom',
-                                                },
-                                            },
-                                        }],
-                                        xaxis: {
-                                            categories:['8:21','8:22','8:23','8:24','8:25','8:26','8.27']
-                                        },
-                                        colors: ['#1f77b4', '#ff0000']
-                                    
-                                    }}
-                                />
-                            </div>
                         </ol>
+                    </div>
+                    <div id="Packet-chart-container">
+                        <div id="chart">
+                            <div id="network" style={{ width: '1175px', boxShadow: '11px -16px 10px rgba(0, 0, 0, 0.1)' }}>
+                                <ReactApexChart
+                                    options={chart1.options}
+                                    series={chart1.series}
+                                    type="area"
+                                    height={280} />
+                            </div>
+                            <div id="at_types" style={{ width: '570px', marginTop: '15px', boxShadow: '11px -16px 10px rgba(0, 0, 0, 0.1)' }}>
+                                <ReactApexChart
+                                    options={chart2.options}
+                                    series={chart2.series}
+                                    type="radar"
+                                    height={350} />
+                            </div>
+                            <div id="at_risk" style={{ width: '570px', marginTop: '-366px', marginLeft: '604px', boxShadow: '11px -16px 10px rgba(0, 0, 0, 0.1)' }}>
+                                <ReactApexChart
+                                    options={chart3.options}
+                                    series={chart3.series}
+                                    type="pie"
+                                    height={350} />
+                            </div>
+                            <div id="at_count" style={{ width: '1175px', marginTop: '30px', boxShadow: '11px -16px 10px rgba(0, 0, 0, 0.1)' }}>
+                                <ReactApexChart
+                                    options={chart4.options}
+                                    series={chart4.series}
+                                    type="bar"
+                                    height={380} />
+                            </div>
+                        </div>
                     </div>
                 </main>
             </div>
